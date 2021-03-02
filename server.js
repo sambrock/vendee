65c3e3ab99519becbdf5c26f6d6885602bb9c95d
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-03-02 22:43:13 +0000

    Added env variables.

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('HI');
-});
+// Use routes
+app.use('/api/occupancy', require('./routes/api/occupancy'));
+
+// DB Config
+const db = process.env.LOCAL_DB_CONFIG;
+
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Error', err));
 
 const port = process.env.PORT || 3001;
+
 app.listen(port, () => console.log(`Server listening on ${port}`));
