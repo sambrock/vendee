b9d15b24c8ab8733b89c4222dc54abf9a074974e
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-03-03 19:40:19 +0000

    Initial commit.

---
+++ client/main.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join('./icon/icon.ico'),
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+});
