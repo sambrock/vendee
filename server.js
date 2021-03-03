24b4eee69f39fde3a043f73c05b73547276d2636
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-03-03 18:10:54 +0000

    Removed port / db config to env variable.

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ app.use(express.json());
 // Use routes
 app.use('/api/occupancy', require('./routes/api/occupancy'));
 
-// DB Config
-const db = process.env.LOCAL_DB_CONFIG;
-
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.LOCAL_DB_CONFIG, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error', err));
+  .catch(err => console.error('Error', err));
 
-const port = process.env.PORT || 3001;
-
+// Server
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server listening on ${port}`));
