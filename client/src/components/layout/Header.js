ea40a563b3926208147b7d8d7d1840fd4a04de78
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-03-20 19:45:07 +0000

    Updates time.

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,10 +1,24 @@
-import { DateTime } from "luxon";
+import { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
 
-const Header = () => (
-  <header className="px-6 py-9 font-medium text-blackOpacity h-auto">
-    <span className=" mr-6">{DateTime.now().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' })}</span>
-    <span className="">{DateTime.now().toLocaleString({ hour: '2-digit', minute: '2-digit', hour12: false })}</span>
-  </header>
-)
+const Header = () => {
+  const [date, setDate] = useState();
+  
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      setDate(Date.now());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [])
+
+  if(!date) return <div></div>;
+
+  return (
+    <header className="px-6 py-9 font-medium text-blackOpacity h-auto">
+      <span className=" mr-6">{DateTime.fromMillis(date).toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' })}</span>
+      <span className="">{DateTime.fromMillis(date).toLocaleString({ hour: '2-digit', minute: '2-digit', hour12: false })}</span>
+    </header>
+  )
+}
 
 export default Header;
