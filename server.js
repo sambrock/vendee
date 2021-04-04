b7433681723edb16333353f7ef29fa1ba7c89be8
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-04-04 18:30:25 +0100

    Serve static assets in production.

--- server.js
+++ server.js
@@ -19,9 +19,19 @@ app.use('/api/products', require('./routes/api/products'));
 app.use('/test', require('./routes/test/test'));
 
 // Connect to MongoDB
-mongoose.connect(process.env.DB_CONFIG, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.LOCAL_DB_CONFIG_TEST, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error', err));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Server
 const port = process.env.PORT;
