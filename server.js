e51e63baf58cb011ed996a62e20bf001ca93293a
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-04-06 12:57:34 +0100

    test

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.use('/api/discounts', require('./routes/api/discounts'));
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_CONFIG, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error', err));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
