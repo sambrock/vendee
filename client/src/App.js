15dca8689291600ce2d1c42ca499d91009326f63
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-04-06 12:43:22 +0100

    Cleanup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import Dashboard from './pages/Dashboard';
