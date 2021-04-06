796f15e1a9a4b1d636605ef7e54773c1f4d93a7f
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-04-06 23:04:58 +0100

    Added comments for routes.

--- routes/api/discounts.js
+++ routes/api/discounts.js
@@ -5,12 +5,18 @@ const auth = require('../../middleware/auth');
 
 const router = express.Router();
 
+// @route   GET api/discounts
+// @desc    List all discounts
+// @access  Private
 router.get('/', auth, async (req, res) => {
   const discounts = await Discount.find({});
 
   res.send(discounts.map(d => {return {...d.toObject(), id: d.discountId}}));
 });
 
+// @route   GET api/discounts/:id
+// @desc    Get discount by id
+// @access  Public
 router.get('/:id', async (req, res) => {
   const discounts = await Discount.find({discountId: req.params.id});
   if(!discounts.length === 0) return res.sendStatus(400)
@@ -18,6 +24,9 @@ router.get('/:id', async (req, res) => {
   res.send(discounts[0]);
 });
 
+// @route   GET api/discounts/code/:id
+// @desc    Get discount by code
+// @access  Public
 router.get('/code/:id', async (req, res) => {
   const discounts = await Discount.find({code: req.params.id});
   if(!discounts.length === 0) return res.sendStatus(400)
