7b273413095b088c0ef298fba4d4b5cb867f96b7
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-04-07 11:22:18 +0100

    Removed auth middleware on list all discounts.

--- routes/api/discounts.js
+++ routes/api/discounts.js
@@ -1,25 +1,24 @@
 const express = require('express');
 
 const Discount = require('../../models/discount');
-const auth = require('../../middleware/auth');
 
 const router = express.Router();
 
 // @route   GET api/discounts
 // @desc    List all discounts
-// @access  Private
-router.get('/', auth, async (req, res) => {
+// @access  Public
+router.get('/', async (req, res) => {
   const discounts = await Discount.find({});
 
-  res.send(discounts.map(d => {return {...d.toObject(), id: d.discountId}}));
+  res.send(discounts.map(d => { return { ...d.toObject(), id: d.discountId } }));
 });
 
 // @route   GET api/discounts/:id
 // @desc    Get discount by id
 // @access  Public
 router.get('/:id', async (req, res) => {
-  const discounts = await Discount.find({discountId: req.params.id});
-  if(!discounts.length === 0) return res.sendStatus(400)
+  const discounts = await Discount.find({ discountId: req.params.id });
+  if (!discounts.length === 0) return res.sendStatus(400)
 
   res.send(discounts[0]);
 });
@@ -28,8 +27,8 @@ router.get('/:id', async (req, res) => {
 // @desc    Get discount by code
 // @access  Public
 router.get('/code/:id', async (req, res) => {
-  const discounts = await Discount.find({code: req.params.id});
-  if(!discounts.length === 0) return res.sendStatus(400)
+  const discounts = await Discount.find({ code: req.params.id });
+  if (!discounts.length === 0) return res.sendStatus(400)
 
   res.send(discounts[0]);
 });
